e0fe3b53de7c85a15bfaa1c4d52b274ce3a0b3b8
JoaoRaposo <jrraposo@poli.ufrj.br>
2014-06-28 16:52:05 -0300

    Criada a Classe LighSensor

---
+++ LightSensor.h
@@ -0,0 +1,53 @@
+#ifndef LIGHT_SENSOR_H
+#define LIGHT_SENSOR_H
+#include "Arduino.h"
+class LightSensor
+{
+  public:
+  void Init(unsigned short port);
+  float getValue();
+  void setMin(unsigned m);
+  void setMax(unsigned m);
+  unsigned getMin(){return minRead;};
+  unsigned getMax(){return maxRead;};
+  void setWeight(float w);
+  int getRawValue();
+  private:
+  unsigned short port;
+  unsigned minRead;
+  unsigned maxRead;
+  float weight;
+  float value;
+};
+void LightSensor::Init(unsigned short port)
+{
+  this->port = port;
+  minRead = 1024;
+  maxRead = 0;
+  weight = 1.0;
+}
+void LightSensor::setMin(unsigned m)
+{
+  minRead = m;
+}
+void LightSensor::setMax(unsigned m)
+{
+  maxRead = m;
+}
+void LightSensor::setWeight(float w)
+{
+  weight = w;
+}
+int LightSensor::getRawValue()
+{
+  return analogRead(port);
+}
+
+float LightSensor::getValue()
+{
+  value = analogRead(port);
+  value = (value - minRead) * 1000.0 / (maxRead - minRead);
+  return weight * value;
+}
+
+#endif
--- Model.h
+++ Model.h
@@ -4,6 +4,7 @@
 #include "Arduino.h"
 #include "Const.h"
 #include "Controler.h"
+#include "LightSensor.h"
 
 class Model
 {
@@ -14,22 +15,17 @@ public:
   void printSensors();
 private:
   Controler control;
-  float sensors[NUMBER_OF_SENSORS];
-  float classSensors[NUMBER_OF_SENSORS][2];
-  float weigth[3];
-  float minMaxSensors[NUMBER_OF_SENSORS][2];
+  LightSensor sensors[NUMBER_OF_SENSORS];
   float centerOfMass;
-
   void readSensors();
   void getMinMax();  
 };
 
 void Model::Init()
 {
-  for( int i = 0; i < NUMBER_OF_SENSORS; i++)
+  for(int i  = 0; i < NUMBER_OF_SENSORS; i++)
   {
-    minMaxSensors[i][0] = 1024;
-    minMaxSensors[i][1] = 0;
+    sensors[i].Init(i);
   }
   getMinMax();
 }
@@ -40,14 +36,13 @@ float Model::getCenterOfMass(){
 
 void Model::readSensors(){
   for(int i = 0;i < NUMBER_OF_SENSORS;i++){
-    sensors[i] = map(analogRead(i),minMaxSensors[i][0],minMaxSensors[i][1],0,1000);
+    sensors[i].getValue();
   }
 }
 
 void Model::printSensors(){
-  readSensors();
   for(int i = 0;i < NUMBER_OF_SENSORS;i++){
-    Serial.print(sensors[i]);
+    Serial.print(sensors[i].getValue());
     Serial.print("\t");
   }
   Serial.println();
@@ -58,27 +53,27 @@ void Model::getMinMax(){
    control.rotateRight(vel);
   do{
     for(int i = 0; i < NUMBER_OF_SENSORS;i++){
-      sensors[i] = analogRead(i);
-      if(sensors[i] < minMaxSensors[i][0]) minMaxSensors[i][0] = sensors[i];
-      if(sensors[i] > minMaxSensors[i][1]) minMaxSensors[i][1] = sensors[i];
+      int actualRead = analogRead(i);
+      if(actualRead < sensors[i].getMin())sensors[i].setMin(actualRead);
+      if(actualRead > sensors[i].getMax())sensors[i].setMax(actualRead);
     }
-  }while(sensors[0] < 850);
+  }while(sensors[0].getRawValue() < 850);
   
   control.rotateLeft(vel);
   do{
     for(int i = 0; i < NUMBER_OF_SENSORS;i++){
-      sensors[i] = analogRead(i);
-      if(sensors[i] < minMaxSensors[i][0]) minMaxSensors[i][0] = sensors[i];
-      if(sensors[i] > minMaxSensors[i][1]) minMaxSensors[i][1] = sensors[i];
+      int actualRead = analogRead(i);
+      if(actualRead < sensors[i].getMin())sensors[i].setMin(actualRead);
+      if(actualRead > sensors[i].getMax())sensors[i].setMax(actualRead);
     }
 
-  }while(sensors[NUMBER_OF_SENSORS - 1] < 970);
+  }while(sensors[NUMBER_OF_SENSORS - 1].getRawValue() < 970);
   control.stopMotors();
   // Retornar ao centro - Tem que melhorar isso
   control.rotateRight(vel);
   do{
     readSensors();
-  }while(sensors[3] < 600);
+  }while(sensors[3].getRawValue() < 600);
   control.stopMotors();
 }
 #endif
